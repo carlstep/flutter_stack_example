f2e3cd4e4542016ac31e49cc4b0d2a2ff517740a
carlstep <carlstep333@me.com>
2022-07-26 09:57:22 +0700

    final code for stack example

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Stack(),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 50,
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+            ),
+          ),
+          Positioned(
+            top: 200,
+            left: 50,
+            child: Icon(
+              Icons.check,
+              color: Colors.black,
+              size: 50,
+            ),
+          ),
+        ],
+      ),
     ),
   ));
 }
